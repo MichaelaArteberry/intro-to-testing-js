348172f0e28044f88c1aa7c3707b6200a26b02bb
Michaela <michaela.arteberry@gmail.com>
2023-10-18 21:08:59 -0400

    REDONE exercise 1 and 2

--- code.js
+++ code.js
@@ -4,18 +4,5 @@ function helloWorld() {
 }
 
 function sayHello(name) {
-    return "Hello, ";
-}
-
-function sayHello(name) {
-    if (name === "Hello") {
-        return "Hello, World!";
-    } else if (name === "Jane") {
-        return "Hello, Jane!";
-    } else if (name === "Alex") {
-        return "Hello, Alex!";
-    } else (name === "Pat")
-    {
-        return "Hello, Pat!";
-    }
+    return " ";
 }
--- tests.js
+++ tests.js
@@ -6,10 +6,18 @@ describe('helloWorld', function () {
     it('should return a string when called', function () {
         expect(typeof helloWorld()).toBe("string");
     });
-    it('should return the string "Hello, Pat!" when executed', function () {
-        expect(sayHello(name)).toBe("Hello, Pat!");
+    it('should return the string "Hello, World!" when executed', function () {
+        expect(helloWorld()).toBe('Hello, World!');
     });
     it("should never return 'undefined' when called", function () {
         expect(helloWorld()).not.toBe(undefined);
     });
 });
+describe('sayHello', function() {
+    it('should be a defined function', function () {
+        expect(typeof sayHello).toBe('function');
+    });
+    it('should return a string when called', function () {
+        expect(typeof sayHello).toBe('string ');
+    });
+})
